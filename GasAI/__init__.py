fa38f55118cfc29874286dda4e29a89cee37b7ba
bafanaS <snbafana@gmail.com>
2024-02-06 10:36:16 -0500

    new

--- GasAI/__init__.py
+++ GasAI/__init__.py
@@ -1,4 +1,4 @@
 from .Communications import Schema
-from .Nodes import Agent, Assistant, Developer, Decider, SplitJoinPair, Splitter, Joiner, Chat, UtilityNode, SplitJoinPair, User, GoalMaker
+from .Nodes import Agent, Assistant, Developer, Decider, SplitJoinPair, Splitter, Joiner, Chat, UtilityNode, SplitJoinPair, User, GoalMaker, Node
 from .Tools import ExecuteCommand, CreateFile, Program, GetFilesInDirectory, OpenFile, CreateDir, SearchWebDDGS, SearchWebGOOGLE, SiteScraper, LinkedINSearch
 from .Tools import ToolImporter, ToolNameDict, GetCurrentDirectory
